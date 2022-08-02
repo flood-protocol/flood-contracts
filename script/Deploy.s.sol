a5d6749c730195b655078a07a7e298b1eeb3c20e
fulminmaxi <francesco@fulminlabs.org>
2022-08-02 11:44:49 +0200

    feat(contracts): add new deploy script
    Signed-off-by: fulminmaxi <francesco@fulminlabs.org>

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -5,21 +5,23 @@ import "src/AllKnowingOracle.sol";
 import "src/BookSingleChain.sol";
 import "forge-std/Script.sol";
 import "forge-std/Test.sol";
+import {Solenv} from "solenv/Solenv.sol";
 
 contract DeployScript is Script, Test {
     AllKnowingOracle internal oracle;
     BookSingleChain internal book;
 
     function run() public {
-        string memory RPC_URL = vm.envString("DEPLOY_RPC_URL");
-        vm.createSelectFork(RPC_URL);
+        Solenv.config();
         uint256 safeBlockThreshold = vm.envUint("SAFE_BLOCK_THRESHOLD");
         uint256 disputeBondPct = vm.envUint("DISPUTE_BOND_PCT");
         uint256 tradeRebatePct = vm.envUint("TRADE_REBATE_PCT");
         uint256 relayerRefundPct = vm.envUint("RELAYER_REFUND_PCT");
         address USDC = vm.envAddress("USDC_ADDRESS");
         address WETH = vm.envAddress("WETH_ADDRESS");
-
+        string memory RPC_URL = vm.envString("DEPLOY_RPC_URL");
+        vm.createSelectFork(RPC_URL);
+        vm.startBroadcast();
         deployOracle();
         deployBook(
             address(oracle),
@@ -29,15 +31,21 @@ contract DeployScript is Script, Test {
             relayerRefundPct
         );
         whitelistTokenForBookAndOracle(
-            address(oracle), address(book), USDC, true
+            address(oracle),
+            address(book),
+            USDC,
+            true
         );
         whitelistTokenForBookAndOracle(
-            address(oracle), address(book), WETH, true
+            address(oracle),
+            address(book),
+            WETH,
+            true
         );
+        vm.stopBroadcast();
     }
 
     function deployOracle() public {
-        vm.broadcast();
         oracle = new AllKnowingOracle();
     }
 
@@ -47,9 +55,7 @@ contract DeployScript is Script, Test {
         uint256 _disputeBondPct,
         uint256 _tradeRebatePct,
         uint256 _relayerRefundPct
-    )
-        public
-    {
+    ) public {
         assertEq(
             _disputeBondPct + _tradeRebatePct + _relayerRefundPct,
             100,
@@ -59,7 +65,7 @@ contract DeployScript is Script, Test {
         assert(_tradeRebatePct > 0);
         assert(_relayerRefundPct > 0);
         assert(_safeBlockThreshold > 0);
-        vm.startBroadcast();
+
         book = new BookSingleChain(
             _oracle,
             _safeBlockThreshold,
@@ -68,7 +74,6 @@ contract DeployScript is Script, Test {
             _relayerRefundPct
         );
         AllKnowingOracle(_oracle).whitelistRequester(address(book), true);
-        vm.stopBroadcast();
     }
 
     function whitelistTokenForBookAndOracle(
@@ -76,12 +81,8 @@ contract DeployScript is Script, Test {
         address _book,
         address _token,
         bool _enable
-    )
-        public
-    {
-        vm.startBroadcast();
+    ) public {
         AllKnowingOracle(_oracle).whitelistToken(_token, _enable);
         AllKnowingOracle(_book).whitelistToken(_token, _enable);
-        vm.stopBroadcast();
     }
 }
