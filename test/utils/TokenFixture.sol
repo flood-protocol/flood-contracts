25245c9c581a172909479992236f32311393fe3d
fulminmaxi <francesco@fulminlabs.org>
2022-09-02 16:40:15 +0200

    chore(contracts): update foundry
    Signed-off-by: fulminmaxi <francesco@fulminlabs.org>

--- test/utils/TokenFixture.sol
+++ test/utils/TokenFixture.sol
@@ -8,40 +8,32 @@ address constant MAINNET_USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48;
 address constant MAINNET_WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
 
 contract MockToken is ERC20 {
-    constructor(string memory name, string memory symbol, uint8 decimals)
-        ERC20(name, symbol, decimals)
-    {}
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) ERC20(name, symbol, decimals) {}
 }
 
 contract TokenFixture is Test {
-    address internal USDC = deployIfNotEmpty(
-        MAINNET_USDC,
-        "TokenFixture.sol:MockToken",
-        abi.encode("USDC", "USDC", 6)
-    );
-    address internal WETH = deployIfNotEmpty(
-        MAINNET_WETH,
-        "TokenFixture.sol:MockToken",
-        abi.encode("Wrapped Ether", "WETH", 18)
-    );
+    address internal USDC =
+        deployERC20IfNotEmpty(MAINNET_USDC, "USDC", "USDC", 6);
+    address internal WETH =
+        deployERC20IfNotEmpty(MAINNET_WETH, "Wrapped Ether", "WETH", 18);
 
     // Deploys a contract to `target` if the address has no existing code.
     // This is used to deploy contracts in tests ONLY if you're not forking.
-    function deployIfNotEmpty(
+    function deployERC20IfNotEmpty(
         address target,
-        string memory whatContract,
-        bytes memory args
-    )
-        internal
-        returns (address)
-    {
+        string memory name,
+        string memory symbol,
+        uint8 decimals
+    ) internal returns (address) {
         uint256 existingCode = target.code.length;
         if (existingCode > 0) {
             return target;
         }
-        address deployed = deployCode(whatContract, args);
-
-        ERC20(deployed).symbol();
-        return deployed;
+        ERC20 deployed = new MockToken(name, symbol, decimals);
+        return address(deployed);
     }
 }
