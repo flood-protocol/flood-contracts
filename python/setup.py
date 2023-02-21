f055a65c42fda5db3317cb4d95d0451d14d6e2c4
Nick Erokhin <nick@fulminlabs.org>
2023-02-21 14:18:34 -0500

    locally working build and workflow

--- python/setup.py
+++ python/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 from os import environ
 
 setup(
-    name='flood-abi',
+    name='flood_contract_abi',
     version=environ['GIT_TAG'],
     license='MIT',
     author="Nick Erokhin",
     author_email='nick@fulminlabs.org',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    packages=['flood_contract_abi'],
+    package_data={'flood_contract_abi': ['abis/*.json']},
     url='',
     keywords='',
     install_requires=[],
