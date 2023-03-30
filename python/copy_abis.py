62ec92f24f290312b1834a70e9574a2be17dc668
Klaudiusz Dembler <accounts@kdembler.com>
2023-03-30 11:46:00 +0200

    fix python bindings script

--- python/copy_abis.py
+++ python/copy_abis.py
@@ -5,10 +5,10 @@ import shutil
 contract_dirs = [d for d in os.listdir('../out') if d.endswith('.sol')]
 
 for d in contract_dirs:
-    contract_dir = os.listdir(os.path.join('../out', d))[0]
-    contract_name = contract_dir[:-5]
-    contract_path = os.path.join('../out', d, contract_dir)
+    contract_files = os.listdir(os.path.join('../out', d))
+    for contract_file in contract_files:
+        contract_name = contract_file[:-5]
+        contract_path = os.path.join('../out', d, contract_file)
 
-    shutil.copyfile(contract_path, 'flood_contract_abi/abis/' + contract_dir)
-
-
+        shutil.copyfile(
+            contract_path, 'flood_contract_abi/abis/' + contract_file)
