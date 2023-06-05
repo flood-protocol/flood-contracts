b651176706614b66c37cee227e8f7f0d90ee8464
Francesco <francesco@fulminlabs.org>
2023-06-05 16:17:55 +0200

    chore(contracts): cleanup for new contracts
    Signed-off-by: Francesco <francesco@fulminlabs.org>

--- src/interfaces/IFloodRegistry.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.8.0;
-
-interface IFloodRegistry {
-    function isAddressWhitelisted(address token) external view returns (bool);
-    function whitelistToken(address token, bool isWhitelisted) external;
-    function batchWhitelistTokens(address[] memory tokens, bool[] memory isWhitelisted) external;
-}
--- test/script/Deploy.t.sol
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
-
-import {DeployScript} from "script/Deploy.s.sol";
-
-contract DeployScriptTest is DeployScript {}
