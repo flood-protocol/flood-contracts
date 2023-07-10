018d8beaedc6ca61afccb4a1b82e6b1e55c4b2a3
Francesco <francesco@fulminlabs.org>
2023-07-10 16:38:10 +0200

    Revert "fix(contracts): move returndatacopy"

--- src/fulfiller/Fulfiller.sol
+++ src/fulfiller/Fulfiller.sol
@@ -339,13 +339,12 @@ contract Fulfiller is IFulfiller, IFulfillerWithCallback, Ownable2Step, Pausable
                 // out and outsize are 0 because we don't know the size yet.
                 let result := delegatecall(gas(), executor, 0, calldatasize(), 0, 0)
 
+                // Copy the returned data.
+                returndatacopy(0, 0, returndatasize())
+
                 switch result
                 // delegatecall returns 0 on error.
-                case 0 {
-                    // Copy the returned data.
-                    returndatacopy(0, 0, returndatasize())
-                    revert(0, returndatasize())
-                }
+                case 0 { revert(0, returndatasize()) }
                 default { return(0, returndatasize()) }
             }
         }
