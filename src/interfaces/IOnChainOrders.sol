6ce4d3664719b8022ec3ee8339799e8977305469
Francesco <francesco@fulminlabs.org>
2023-12-03 15:16:33 +0100

    update order etch event signature
    Signed-off-by: Francesco <francesco@fulminlabs.org>

--- src/interfaces/IOnChainOrders.sol
+++ src/interfaces/IOnChainOrders.sol
@@ -4,12 +4,12 @@ pragma solidity >=0.8.0;
 import {IFloodPlain} from "./IFloodPlain.sol";
 
 interface IOnChainOrders {
-    event OrderEtched(bytes32 indexed orderHash, IFloodPlain.SignedOrder orderWithSignature);
+    event OrderEtched(bytes32 indexed orderHash, IFloodPlain.SignedOrder signedOrder);
 
     /**
      * @notice Record an order on-chain for ease of use by other contracts.
      *
-     * @param orderWithSignature The order and its signature to record.
+     * @param signedOrder The order and its signature to record.
      */
-    function etchOrder(IFloodPlain.SignedOrder calldata orderWithSignature) external;
+    function etchOrder(IFloodPlain.SignedOrder calldata signedOrder) external;
 }
