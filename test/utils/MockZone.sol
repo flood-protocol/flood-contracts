7353b6aa7f21da7a6ea5bc68d62bd7ccae96dd6e
ZeframLou <zeframlou@gmail.com>
2024-10-29 19:52:41 +0800

    update solidity version to 0.8.25

--- test/utils/MockZone.sol
+++ test/utils/MockZone.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.23;
+pragma solidity ^0.8.23;
 
 import {IZone, IFloodPlain} from "src/interfaces/IZone.sol";
 import {Ownable, Ownable2Step} from "@openzeppelin/access/Ownable2Step.sol";
